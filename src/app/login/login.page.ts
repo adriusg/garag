bd8b4db61dc877749627d71dcb288815638f39f5
adriusg <adriusg@gmail.com>
2019-10-29 16:41:28 +0000

    Privacy Page ajust

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import {PrivacyPage} from '../privacy/privacy.page';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) {}
+
+  async movePrivacy() {
+    const modal = await this.modalCtrl.create({
+     component: PrivacyPage
+   });
+
+    return await modal.present();
+  }
 
   ngOnInit() {
   }
